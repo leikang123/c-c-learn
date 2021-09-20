3ab5c57fb5d917b381334e4507435fedc7a91570
leikang123 <1013626230@qq.com>
2021-09-20 18:31:01 +0800

    demo

---
+++ lk03/cmake-build-debug/CMakeFiles/lk03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk03.dir/main.cpp.o"
+  "lk03"
+  "lk03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk03/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+   int a = 10;
+   int &b = a;
+   int &c = b;
+   int &d = a;
+    cout<<"a id value" << a << endl;
+    cout <<" b is value " << b << endl;
+    cout << " c is value " << c << endl;
+    cout << " d is value " << d << endl;
+}
