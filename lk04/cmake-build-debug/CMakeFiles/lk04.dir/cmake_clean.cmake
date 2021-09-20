bf69a2052fe7e2f2c846d0d56994085a86e2283e
leikang123 <1013626230@qq.com>
2021-09-20 18:31:33 +0800

    demo

---
+++ lk04/cmake-build-debug/CMakeFiles/lk04.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk04.dir/main.cpp.o"
+  "lk04"
+  "lk04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk04/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+/**
+ *chan常量的指针
+ */
+using namespace std;
+
+int main() {
+    const int a = 12;
+    const int * x = &a;
+    int b = 23;
+    const int *y = &b;
+    cout << x << endl;
+    cout <<  y << endl;
+
+}
