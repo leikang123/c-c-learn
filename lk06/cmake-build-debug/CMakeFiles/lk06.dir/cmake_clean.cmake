114a3046d752b37e758628051b94479a3087e47d
leikang123 <1013626230@qq.com>
2021-09-20 18:32:54 +0800

    demo

---
+++ lk06/cmake-build-debug/CMakeFiles/lk06.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk06.dir/main.cpp.o"
+  "lk06"
+  "lk06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk06/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    double a[]={1.1,2.2,3.3,4.4,5.5};
+    int b[4];
+    copy(a,a+4, ostream_iterator<double>(cout," "));
+    cout <<endl;
+    reverse_copy(a,a+4, ostream_iterator<double>(cout," "));
+    cout << endl;
+    copy(a,a+4,b);
+    copy(b,b+4,ostream_iterator<double>(cout," "));
+    cout << endl;
+    sort(a,a+4);
+    copy(a,a+4,ostream_iterator<double>(cout," "));
+    cout <<endl;
+    reverse_copy(a,a+4,b);
+    copy(b,b+4,ostream_iterator<double>(cout," "));
+    cout << endl;
+
+int x =58,y=98,z=55;
+int * const p = &x;
+* p=65;
+z = *p;
+cout << p << endl;
+
+}
