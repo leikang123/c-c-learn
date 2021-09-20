ea48b7b1681e25e821b700140deeb228241d55ab
leikang123 <1013626230@qq.com>
2021-09-20 18:32:19 +0800

    demo

---
+++ lk05/cmake-build-debug/CMakeFiles/lk05.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk05.dir/main.cpp.o"
+  "lk05"
+  "lk05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk05/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int x = 12;
+    const int *a = &x;
+    int y = 34;
+    const int *b = &y;
+    cout << a << endl;
+    cout << b << endl;
+
+
+
+}
