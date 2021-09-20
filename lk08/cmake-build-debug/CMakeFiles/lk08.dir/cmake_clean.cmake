3038f76a9b2fcd6f300596c99e5a52a5bf829ea4
leikang123 <1013626230@qq.com>
2021-09-20 18:34:14 +0800

    demo

---
+++ lk08/cmake-build-debug/CMakeFiles/lk08.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk08.dir/main.cpp.o"
+  "lk08"
+  "lk08.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk08/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    string str = "wearhere", str2(str);
+    reverse(str.begin(), str.end());
+    cout << str << endl;
+    copy(str.begin(),str.end(),str2.begin());
+    sort(str.begin(),str.end());
+    cout << str << endl;
+    cout << str2 << endl;
+    reverse_copy(str.begin(),str.end(),str2.begin());
+    cout << str2 << endl;
+
+    reverse(str2.begin()+2,str2.begin()+8);
+    copy(str2.begin()+2,str2.begin()+8,ostream_iterator<char>(cout));
+    cout << endl;
+
+
+    cout << "the end++++++++++++++++" << endl;
+    string str3 [] ={"leikang","wangxiang","liyuty"};
+    for(int i=0;i<3;i++){
+        copy(str3[i].begin(),str3[i].end(),ostream_iterator<char>(cout));
+        cout << str3 << endl;
+
+        str3[0].swap(str3[1]);
+        str3[0].swap(str3[2]);
+        for(int i =0;i<3;i++){
+            cout << str[i]<< endl;
+        }
+    }
+
+}
