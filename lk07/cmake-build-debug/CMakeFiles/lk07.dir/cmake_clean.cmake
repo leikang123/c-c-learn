97f23f065b1b327b2aa5a904912796f1006d33f0
leikang123 <1013626230@qq.com>
2021-09-20 18:33:32 +0800

    demo

---
+++ lk07/cmake-build-debug/CMakeFiles/lk07.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk07.dir/main.cpp.o"
+  "lk07"
+  "lk07.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk07/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+struct  student {
+    void Setxy(double a,double b){
+        x=a;
+        y=b;
+    }
+    void Display(){
+        cout << x << "\t" << y << endl;
+    }
+    double x,y;
+};
+int main() {
+    student a;
+    a.Setxy(10.6,18.5);
+    a.Display();
+    cout << a.x <<"\t" <<a.y << endl;
+}
