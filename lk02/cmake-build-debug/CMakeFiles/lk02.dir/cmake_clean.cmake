c1f3301916e1f65059b58c2d295b188d4f5892de
leikang123 <1013626230@qq.com>
2021-09-20 18:20:57 +0800

    demo

---
+++ lk02/cmake-build-debug/CMakeFiles/lk02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk02.dir/main.cpp.o"
+  "lk02"
+  "lk02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk02/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+/**
+ * 动态分配内存
+ * @return
+ */
+ using namespace std;
+int  main() {
+    // 声明double指针
+    double * p;
+    // 分配3个double数据存储空间
+     p = new double [3];
+     for(int i = 0;i<3;i++){
+         cin >> *(p+i);
+         for(int i=0;i<3;i++){
+            cout <<*(p+i) << "";
+     delete p;
+        }
+     }
+}
