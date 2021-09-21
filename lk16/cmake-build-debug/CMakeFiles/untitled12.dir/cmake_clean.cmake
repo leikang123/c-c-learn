3cc1ef115670939f0377213a9b7c13e04cd1ce47
leikang123 <1013626230@qq.com>
2021-09-21 14:22:35 +0800

    demo

---
+++ lk16/cmake-build-debug/CMakeFiles/untitled12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled12.dir/library.c.o"
+  "libuntitled12.a"
+  "libuntitled12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk16/library.c
@@ -0,0 +1,7 @@
+﻿#include "library.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
