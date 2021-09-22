f5f43d53e34011736cb375ae292a5e40fa63bd1f
leikang123 <1013626230@qq.com>
2021-09-22 16:48:45 +0800

    demo

---
+++ lk28/cmake-build-debug/CMakeFiles/lk28.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk28.dir/main.cpp.o"
+  "lk28"
+  "lk28.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk28.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk28/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+/**
+ * 返回引用的函数
+ * @return
+ */
+using namespace std;
+// 定义数组a
+int a  =1;
+// 声明返回引用的函数
+int& index(int i);
+int main() {
+    index(3) = 16;
+    cout << index(3) << endl;
+
+}
+// 函数的定义
+int& index(int i){
+return a;
+}
