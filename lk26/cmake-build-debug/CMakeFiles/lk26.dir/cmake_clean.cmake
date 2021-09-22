bcaa20bb53c9201d7052484e0994d79294296922
leikang123 <1013626230@qq.com>
2021-09-22 16:46:55 +0800

    demo

---
+++ lk26/cmake-build-debug/CMakeFiles/lk26.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk26.dir/main.cpp.o"
+  "lk26"
+  "lk26.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk26.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk26/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+// 定义对象指针，string类的指针
+void swap (string *,string *);
+int main() {
+    string str1("leikang");
+    string str2("wangli");
+    cout << "str1=" << str1 << endl;
+    cout << "str2=" << str2 << endl;
+    // 传递指针
+    swap(&str1,&str2);
+}
+void swap(string *s1 ,string *s2){
+    string tem = *s1;
+    *s1 =*s2;
+    *s2 = tem;
+    cout << "s1=" << *s1 << endl;
+    cout << "s2=" << *s2 << endl;
+}
