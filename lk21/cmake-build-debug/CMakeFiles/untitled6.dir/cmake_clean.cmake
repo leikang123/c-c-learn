34e383f006dfb0538d2d4ed2846a59ca732d9803
leikang123 <1013626230@qq.com>
2021-09-22 16:43:37 +0800

    demo

---
+++ lk21/cmake-build-debug/CMakeFiles/untitled6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled6.dir/main.c.o"
+  "untitled6"
+  "untitled6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
