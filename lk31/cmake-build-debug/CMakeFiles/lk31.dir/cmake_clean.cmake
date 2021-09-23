b32b9466e699fc79397a07aba75eaeda7526c2c0
leikang123 <1013626230@qq.com>
2021-09-23 17:41:36 +0800

    demo

---
+++ lk31/cmake-build-debug/CMakeFiles/lk31.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk31.dir/main.c.o"
+  "lk31"
+  "lk31.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lk31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk31/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main() {
+    int i,j,k;
+    for(int i=0;i<100;i++){
+        for(int j=0;j<100;j++){
+            for (int k =0;k<100;k++){
+                if(i+j+k ==100 && i*5+j*3+k/3.0 ==100)
+                printf("%d     %d      %d      \n",i,j,k);
+            }
+        }
+    }
+}
