50c52ccc2756c1c6551813e12a20aed377815b76
leikang123 <1013626230@qq.com>
2021-09-24 19:20:01 +0800

    demo

---
+++ lk37/cmake-build-debug/CMakeFiles/lk37.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk37.dir/main.cpp.o"
+  "lk37"
+  "lk37.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk37.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk37/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+// 定义两个类
+class Point{
+private :
+    int x,y;
+public :
+    void Set(int a,int b){
+    x=a,y=b;
+}
+int Getx(){
+    return x;
+}
+int Gety(){
+    return y;
+}
+};
+class Rectangle{
+    Point Loc;
+    int H,W;
+public :
+    void Set(int x,int y,int h,int w);
+    Point * getLoc();
+    int Getheight(){
+        return H;
+    }
+    int Getweight(){
+        return W;
+    }
+};
+void Rectangle::Set(int x, int y, int h, int w) {
+    Loc.Set(x,y);
+    H=h,W =w;
+}
+int main() {
+   Rectangle rect;
+   rect.Set(10,2,25,20);
+   cout << rect.Getheight() << endl;
+   cout << rect.Getweight() << endl;
+   Point * p = rect.getLoc();
+   cout << p ->Getx() << "," << p ->Gety() << endl;
+}
