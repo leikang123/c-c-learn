1b3f783d8ed81ae46b7d2714563ef13a29c9ec2e
leikang123 <1013626230@qq.com>
2021-09-24 19:21:46 +0800

    demo

---
+++ leikang1/src/boolean.rs
@@ -0,0 +1,17 @@
+﻿
+fn main(){
+    // 定义x为true
+    let x=true;
+    // 定义y为false
+    let y=false;
+    // 定义z的运算 &&
+    let z = x && y;
+    println!("The value of is {}",z);
+    // 定义w的运算
+    let w = x || y;
+    print!("The value of is {}",w);
+    // 定义 m的运算
+    let m =x ^ y;
+    print!("The value of is {}",m)
+
+}
