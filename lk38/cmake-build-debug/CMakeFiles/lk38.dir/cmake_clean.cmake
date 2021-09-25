e74663883ee846292c6e18991ad393602b2527b8
leikang123 <1013626230@qq.com>
2021-09-25 17:56:32 +0800

    demo

---
+++ lk38/cmake-build-debug/CMakeFiles/lk38.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk38.dir/main.cpp.o"
+  "lk38"
+  "lk38.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk38.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk38/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+/**
+ * 对象成员初始化：就是一个类中有其他类的对象成员函数
+ * @return
+ */
+ // 定义本类以及对象成员
+ class Car{
+ private :
+     int color;
+ public :
+     Car() {
+
+         cout << " Not s id" << endl;
+     }
+     Car(int x):color(){
+     cout << " You is largre" << endl;
+ }
+ ~ Car(){
+    cout << "xigou hanshu " << endl;
+}
+ };
+ class House{
+ private :
+     Car size;
+     Car pize;
+     int data;
+ public :
+     House() {
+         cout << "yuis " << endl;
+     }
+     ~House(){
+     cout << " das" << endl;
+ }
+ };
+ House()::House():sizt(),pize(){
+     data = k;
+     cout << "input is hdg" << endl;
+ }
+int main() {
+    House obj,anObj(5,6,10);
+    cout << ""<< endl;
+
+}
