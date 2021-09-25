0e516564bb1aeec7fa71f22cae721aa0ea798d9f
leikang123 <1013626230@qq.com>
2021-09-25 17:58:02 +0800

    demo

---
+++ lk30/main.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main()
+{
+ int n,m,k;
+ printf("请输入 m,n:\n");
+ scanf("%d%d",&m,&n);
+ if (m>n)
+ {
+     k=n;
+ }else
+ {
+     k=m;
+ }
+ while (k >1)
+ {
+     if (m %k ==0 && n%k ==0)
+     {
+         break;
+         k --;
+     }
+ }
+    printf("%d",k);
+}
