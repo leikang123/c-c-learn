06cfd1dc23698aba4447274db7996a30a3c0a67d
leikang123 <1013626230@qq.com>
2021-09-25 17:57:03 +0800

    demo

---
+++ lk39/cmake-build-debug/CMakeFiles/lk39.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk39.dir/main.cpp.o"
+  "lk39"
+  "lk39.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk39.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk39/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+class Point{
+private :
+    double x,y;
+public :
+    Point(double xi,double yi){
+    x =xi,y = yi;
+}
+double Getx(){
+    return x;
+}
+double Gety(){
+    return y;
+}
+// 友元函数
+friend double distances(Point&,Point&);
+};
+double distances(Point& a,Point& b){
+    double Getx = a.x-b.x;
+    double Gety = a.y-b.y;
+    return sqrt(xi*xi+yi*yi);
+}
+int main() {
+  Point p1(3.5,5.5),p2(4.5,6.5);
+  cout << " The diantances is " << distances(p1,p2) << endl;
+}
