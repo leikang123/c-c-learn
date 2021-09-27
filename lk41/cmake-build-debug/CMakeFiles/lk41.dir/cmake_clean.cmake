4948912377080602ac3b29d95fc7d32488fed49f
leikang123 <1013626230@qq.com>
2021-09-27 19:01:10 +0800

    demo

---
+++ lk41/cmake-build-debug/CMakeFiles/lk41.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk41.dir/main.cpp.o"
+  "lk41"
+  "lk41.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk41.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk41/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+class A{
+private :
+    int x,y;
+public :
+    A(int a,int b){
+     x =a;
+     y=b;
+     cout << "x=" << x << endl;
+     cout << "y=" << y  << endl;
+
+};
+void Show(){
+    cout << "show ....." << endl;
+}
+};
+class B : public A{
+private :
+    int z,w;
+public :
+    B(int a,int b,int c,int d):A(a,b){
+    c =z;d=w;
+    cout << "c=" << endl;
+    cout << "d=" << endl;
+
+}
+void Show(){
+    cout << "show2......show2..." << endl;
+
+}
+};
+class C : public B{
+public:
+    C(int a,int b,int c,int d): B(a,b,c,d) {
+    }
+        void Show(){
+            B::Show();
+            cout << "show3......." << endl;
+    }
+};
+
+int main() {
+   A la(2,3);
+   la.Show();
+   B lb(1,5,7,9);
+   C lc (6,7,8,9);
+   lc.Show();
+
+}
