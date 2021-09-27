4a7f59e31a15d93aaa4179cdc0756698b0120617
leikang123 <1013626230@qq.com>
2021-09-27 19:01:41 +0800

    demo

---
+++ lk42/cmake-build-debug/CMakeFiles/lk42.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk42.dir/main.cpp.o"
+  "lk42"
+  "lk42.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk42.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk42/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+class A {
+private :
+    int a,b;
+public :
+    A();
+   A(int x,int y){
+       x=a,y=b;
+       cout << x <<"," << y << endl;
+
+   }
+};
+class B :public A{
+
+};
+class C :public  B,public A{
+
+};
+class D:public C,public  B,public A{
+
+};
+class E :public D,public C,public B,public A{
+
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
