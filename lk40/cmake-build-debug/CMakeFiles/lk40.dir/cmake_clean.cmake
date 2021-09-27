9fbb58abdf4a911cb5fd7f66d34355ac074510ae
leikang123 <1013626230@qq.com>
2021-09-27 18:58:27 +0800

    demo

---
+++ lk40/cmake-build-debug/CMakeFiles/lk40.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk40.dir/main.cpp.o"
+  "lk40"
+  "lk40.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk40.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk40/main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+class Point{
+private :
+    int x,y;
+public :
+    Point(int a,int b){
+    x=a,y=b;
+   cout << "POint ..." << endl;
+}
+void Showxy(){
+    cout << "x=" << x << endl;
+    cout << "y=" << y <<endl;
+}
+~Point(){
+    cout <<"~~Point...." << endl;
+}
+};
+class Res : public Point{
+private :
+    int H,W;
+public :
+    Res(int a,int b,int h,int w): Point(a,b){
+    H=h,W=w;
+    cout << "res.." <<  endl;
+}
+void Show(){
+    cout << "H=" << endl;
+    cout << "w=" << endl;
+}
+~Res(){
+    cout << "~~~~Res....." << endl;
+}
+};
+int main() {
+   Res r(12,23,11,89);
+   r.Show();
+   r.Showxy();
+   // 创建对象
+   Point a(2,1);
+   Res b(111,555,777,999);
+   a.Showxy();
+   // 不能调用子类的函数
+  //  a.Show();
+   b.Show();
+   Point &ra = b;
+   ra.Showxy();
+   Point * p = &b;
+   p ->Showxy();
+   Res * q = &b;
+   q ->Show();
+   q -> Showxy();
+
+
+
+
+
+
+
+}
