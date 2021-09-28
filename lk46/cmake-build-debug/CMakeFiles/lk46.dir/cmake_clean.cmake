ac17d342d144e9778aaf814fe4beae4b08390f2c
leikang123 <1013626230@qq.com>
2021-09-28 17:11:00 +0800

    demo

---
+++ lk46/cmake-build-debug/CMakeFiles/lk46.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk46.dir/main.cpp.o"
+  "lk46"
+  "lk46.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk46.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk46/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace  std;
+template <class T> class A{
+    T a,b,c,d;
+    T Max(T a,T b){
+        return (a>b)? a:b ;
+    }
+public :
+    Max4(T,T,T,T);
+    T Max(void);
+};
+// 声明模版定义成员函数
+template<class T>
+Max4 <T> :: Max4(T x1,T x2,T x3,T x4):a(x1),b(x2),c(x3),d(x4){
+
+}
+template <class T>
+T Max4<T>:: Max(void){
+    return Max(Max(a,b),Max(c,d));
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
