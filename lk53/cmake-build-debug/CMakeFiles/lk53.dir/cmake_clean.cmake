61050b683006c2267cbc2aab248cc6b9e18ec352
leikang123 <1013626230@qq.com>
2021-09-28 17:14:29 +0800

    demo

---
+++ lk53/cmake-build-debug/CMakeFiles/lk53.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk53.dir/main.cpp.o"
+  "lk53"
+  "lk53.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk53.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk53/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+# include <vector>
+
+using namespace std;
+
+int main() {
+    // defin char 10
+    char as[11] ="adfgkirtoc";
+    // 向量复制数组的字符10
+    vector<char>a (as,as+10);
+    // 遍历向量a
+    for(int i =0;i<10;i++)
+        cout << a[i] << " ";
+    cout << endl << a.capacity()<<"," << a.size()<< "," <<a.max_size()<<endl;
+    a.push_back('l');
+    a.push_back('w');
+    a.pop_back();
+    for(int i=0;i<13;i++)
+        cout << a[i] << "," ;
+    cout << a.capacity()<<"," << a.size()<<endl;
+   a.clear();
+   for(int i=0;i<16;i++)
+       cout << a[i] << "," ;
+   a.clear();
+   cout << a.empty() <<endl;
+}
