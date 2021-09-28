f05d8ca208010b087161a13a1b24045e02eb59e7
leikang123 <1013626230@qq.com>
2021-09-28 17:15:11 +0800

    demo

---
+++ lk54/cmake-build-debug/CMakeFiles/lk54.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk54.dir/main.cpp.o"
+  "lk54"
+  "lk54.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk54.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk54/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  char a[10]="qwertyuio";
+  // defin vector
+    vector<char > b (a,a+10);
+    for (int i=0;i<12;i++)
+        cout << b[i] << "," ;
+        cout <<endl;
+        //defin charper
+        vector<char> :: iterator p;
+        p = b.begin();
+        b.insert(p+3,'L');
+        copy(b.begin(),b.end(),ostream_iterator<char>(cout,""));
+        cout << endl;
+        p =b.begin();
+        b.insert(p,3,'K');
+    copy(b.begin(),b.end(),ostream_iterator<char>(cout,""));
+    cout << endl;
+    cout << b.size()<< "," << b.capacity() << endl;
+    p =b.begin();
+    b.insert(p,10,'L');
+    copy(b.begin(),b.end(),ostream_iterator<char>(cout,""));
+    cout << endl;
+    cout << b.size() << "," << b.capacity()<< endl;
+    b.erase(p+8);
+    copy(b.begin(),b.end(),ostream_iterator<char>(cout,""));
+    cout << endl;
+
+
+
+}
+
