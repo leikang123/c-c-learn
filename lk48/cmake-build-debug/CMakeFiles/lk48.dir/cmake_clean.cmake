ce621cff845396b066fd8faa1a408e610692b813
leikang123 <1013626230@qq.com>
2021-09-28 17:11:57 +0800

    demo

---
+++ lk48/cmake-build-debug/CMakeFiles/lk48.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk48.dir/main.cpp.o"
+  "lk48"
+  "lk48.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk48.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk48/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+/**
+ * 类模版继承母类，母类为非模版类
+ */
+class Point{
+private :
+    int x,y;
+public :
+    Point(int a,int b){
+    x=a;y =b;
+}
+void display(){
+    cout << x << "," << y << endl;
+}
+};
+template <class T >
+class Line : public Point{
+    T x2,y2;
+public:
+    Line(int a ,int b,T c, T d): Point(a,b){
+        x2=c;y2=d;
+    }
+    void display(){
+        Point::display();
+        cout << x2 <<"," << y2 << endl;
+    }
+};
+int main() {
+    Point a(3,8);a.display();
+    Line <int> ab(4,5,6,7);
+    ab.display();
+    Line<double> ad(2,3,4.7,9.7);
+    ad.display();
+}
