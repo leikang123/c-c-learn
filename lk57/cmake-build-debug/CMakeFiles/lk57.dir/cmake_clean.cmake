5da2a8a63dc7e3d5f47b358920c90bdc24c02b79
leikang123 <1013626230@qq.com>
2021-09-29 18:34:42 +0800

    demo

---
+++ lk57/cmake-build-debug/CMakeFiles/lk57.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk57.dir/main.cpp.o"
+  "lk57"
+  "lk57.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk57.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk57/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+class A{
+private :
+    int x;
+public :
+    // 构造函数无参数
+    A(){};
+// 虚函数
+virtual void func(){
+    cout << "virtual void func" << endl;
+}
+// 析构函数
+~A(){
+    cout << "~A " << endl;
+}
+// 虚函数
+virtual void fund(){
+    cout << "virual void fund" << endl;
+}
+};
+class B : public A {
+public :
+    // 构造函数无参数
+    B(){
+    func();
+}
+void fun(){
+    cout << "fun " << endl;
+}
+~B(){
+    fund();
+}
+};
+class C :public B{
+public :
+    C(){}
+    void func(){
+    cout << "class C func " << endl;
+}
+~C(){
+    fund();
+}
+void fund(){
+    cout << " c fund" << endl;
+}
+};
+int main() {
+    C c;
+    c.fun();
+}
