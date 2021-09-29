755f442cf08cdadf6f5887a4b5010d3a8a7d65fb
leikang123 <1013626230@qq.com>
2021-09-29 18:36:08 +0800

    demo

---
+++ lk58/cmake-build-debug/CMakeFiles/lk58.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk58.dir/main.cpp.o"
+  "lk58"
+  "lk58.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk58.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk58/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+class A {
+public :
+    virtual void f(){
+    cout << "A is " << endl;
+}
+};
+class B{
+    virtual void f(){
+        cout << "B is " << endl;
+    }
+};
+class C :public B ,public A{
+     void f(){
+        cout << " c is " << endl;
+    }
+};
+
+int main() {
+  // C c;
+   A *pa ;
+   B *pb ;
+   C *pc,c;
+  pa =&c,pb=&c,pc=&c;
+  pa->f();
+  pb ->f();
+  pc ->f();
+}
