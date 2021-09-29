048e5abba85cc8e4a92063a35c36b864470959d1
leikang123 <1013626230@qq.com>
2021-09-29 18:33:56 +0800

    demo

---
+++ lk56/cmake-build-debug/CMakeFiles/lk56.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk56.dir/main.cpp.o"
+  "lk56"
+  "lk56.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk56.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk56/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace  std;
+class A{
+private :
+    int x,y;
+public :
+    A(int a,int b){
+    a  =x,y=b;
+}
+virtual int display( ){
+    return 0;
+}
+};
+class B :public A{
+private :
+    int z;
+public :
+    B(int a,int b,int c):A(a,b){
+    z =c;
+}
+ int display(){
+    return z;
+}
+};
+void sad(A *p) {
+    cout << p->display() << endl;
+
+}
+void sad(A &q){
+    cout << q.display() << endl;
+
+}
+int main() {
+    A a(2,3);
+    B b(5,7,8);
+    A * p =&b;
+    A & q =b;
+    sad(a);
+    sad(p);
+    sad(q);
+}
