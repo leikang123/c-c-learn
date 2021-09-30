07eaabebfc6d69e99fbc2e64bff4961a5a3dbac0
leikang123 <1013626230@qq.com>
2021-09-30 17:58:43 +0800

    demo

---
+++ 33/cmake-build-debug/CMakeFiles/33.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "33"
+  "33.pdb"
+  "CMakeFiles/33.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/33.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 33/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+/**
+ * g构造函数的学习法
+ * @return
+ */
+ class Point{
+ private :
+     int x,y;
+ // 公开的类成员函数
+ public :
+     // 无参数构造函数
+     Point();
+     // 有参数构造函数
+     Point(int a,int b);
+ };
+ // 构造函数，无参数
+ Point::Point():x(0),y(0) {
+     cout << "init" << endl;
+ }
+ // 构造函数有参数
+ Point::Point(int a, int b):x(a),y(b) {
+     cout << "init " << a <<"," << b << endl;
+ }
+int main() {
+     // 声明对象A，无参数
+    Point A;
+    // 声明对象B有参数
+    Point B(13,45);
+    // 声明对象数组，两个元素无参数
+    Point C[2];
+    // 声明类对象D数组有参数两个元素
+    Point D[2] = {
+            Point(6,9),Point(12,17)
+    };
+}
