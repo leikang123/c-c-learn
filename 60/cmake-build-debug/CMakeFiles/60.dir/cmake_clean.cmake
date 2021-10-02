62e0a4389d991fd4c836b4d8b96aaa0e08cda9cd
leikang123 <1013626230@qq.com>
2021-10-02 17:45:34 +0800

    deno

---
+++ 60/cmake-build-debug/CMakeFiles/60.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "60"
+  "60.pdb"
+  "CMakeFiles/60.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/60.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 60/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    double b;
+    char c;
+    float d;
+
+    cin >> a ;
+    cin >> b ;
+    cin >> c;
+    cin >> d;
+    cout << showpoint << 123.0 << "";
+    cout << noshowpoint << 123.0 << "";
+    cout << a << "" << showbase << a << dec << endl;
+
+}
