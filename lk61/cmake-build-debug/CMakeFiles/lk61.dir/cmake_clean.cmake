4a1a3598927d96da2d98deb918c290ef4f9c65cf
leikang123 <1013626230@qq.com>
2021-10-02 17:43:46 +0800

    demo

---
+++ lk61/cmake-build-debug/CMakeFiles/lk61.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk61.dir/main.cpp.o"
+  "lk61"
+  "lk61.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk61.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lk61/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    char ch[15], *p = "abcdefghij";
+    ofstream myFile;
+    myFile.open("text.txt");
+    myFile << p;
+    myFile.close();
+    ifstream getText("Text.txt");
+    for (int i = 0; i < strlen(p) + 8; i++) {
+        getText >> ch[i];
+        ch[i] = '\0';
+        getText.close();
+        cout << ch;
+    }
+
+}
