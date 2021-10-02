fc711424ff4e664ac591cda214ab0278e51a93b6
leikang123 <1013626230@qq.com>
2021-10-02 17:45:04 +0800

    demo

---
+++ 59/cmake-build-debug/CMakeFiles/59.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "59"
+  "59.pdb"
+  "CMakeFiles/59.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/59.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 59/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+class Str{
+private:
+    char *st;
+public :
+    Str(char *s);
+    Str(Str & s);
+    Str & operator=(Str& a);
+    Str& operator =(char *s);
+    void print(){
+        cout << st << endl;
+    }
+~Str(){
+        delete st;
+    }
+};
+Str ::Str(char * s) {
+    st =new char[strlen(s)+1];
+    strcpy(st,s);
+}
+Str ::Str(Str &s) {
+    st =new char[strlen(s.st)+1];
+    strcpy(st,s.st);
+}
+Str & Str ::operator=(Str &a) {
+    if (this == &a)
+        return * this;
+    delete st;
+    st =new char[strlen(a.st)+1];
+    strcpy(st,a.st);
+    return  *this;
+}
+Str & Str ::operator=(Str &s) {
+    delete st;
+    st = new char[strlen(s.st)+1];
+    strcpy(st,s);
+    return * this;
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
