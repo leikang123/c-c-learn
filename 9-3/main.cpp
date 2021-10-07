982e7065875766823c58298710a5497d049b826e
leikang123 <1013626230@qq.com>
2021-10-07 17:14:14 +0800

    demo

---
+++ 9-3/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+class number {
+    int num;
+public :
+    number (int i){
+    num  =i;
+}
+int operator ++();
+int operator++(int );
+void print(){
+    cout << "num =" << num << endl;
+}
+};
+int number ::operator++() {
+    num++;
+    return num;
+}
+int number ::operator++(int ) {
+    int i =num ;
+    num++;
+    return i;
+
+}
+int main() {
+  number n(10);
+  int i = ++n;
+  cout << "i=" << i << endl;
+  n.print();
+  i = n++;
+  cout << "i=" << i << endl;
+  n.print();
+}
