b05ea940c82491cc208c41ade8a8c3cbf77062e8
leikang123 <1013626230@qq.com>
2021-10-07 17:12:31 +0800

    demo

---
+++ 9-2/cmake-build-debug/CMakeFiles/9_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "9_2"
+  "9_2.pdb"
+  "CMakeFiles/9_2.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9-2/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using  namespace std;
+
+class test{
+private :
+    int i;
+    float f;
+    char ch;
+public :
+    test();
+    test(int a, float b,char c) {
+    i = a;
+    f = b;
+    c = ch;
+}
+friend ostream &operator << (ostream & output,test &a );
+friend istream & operator >>  (istream & intput ,test & b);
+};
+ostream & operator << (ostream & output ,test & a){
+    output << a.i << endl;
+    output << a.f << endl;
+    output << a.ch << endl;
+    return output;
+}
+istream & operator >> (istream & intput,test & b){
+    intput >> b.i ;
+    intput >> b.f ;
+    intput >> b.ch;
+    return intput;
+}
+int main() {
+ test A(12,34.6,'ASDF');
+ cout << A;
+ test B,C;
+ cin >> B >> C;
+ cout << B << C;
+
+}
