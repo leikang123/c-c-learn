05d254fafa6c966326d3143028989dfba5de3697
leikang123 <1013626230@qq.com>
2021-10-09 13:55:45 +0800

    demo

---
+++ 9-5/cmake-build-debug/CMakeFiles/9_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "9_5"
+  "9_5.pdb"
+  "CMakeFiles/9_5.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9-5/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+class complex{
+private :
+    double real,imag;
+public :
+    complex(double r =0,double i=0){
+    real =r;
+    imag =i;
+}
+friend complex operator+(complex,complex);
+void show (){
+ cout << real << "+" << imag << "i";
+}
+};
+complex operator+(complex a,complex b){
+    double r = a.real+b.real;
+    double i =a.imag + b.imag;
+    return complex(r,i);
+}
+
+int main() {
+    complex x(5,3),y;
+    y = x+7;
+    y = 7+y;
+    y.show();
+}
