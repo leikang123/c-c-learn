cfa54234c50e8208e11c4c5ecc797a09df3677c9
leikang123 <1013626230@qq.com>
2021-11-08 16:58:32 +0800

    demo

---
+++ thread2/cmake-build-debug/CMakeFiles/thread2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread2.dir/main.cpp.o"
+  "thread2"
+  "thread2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thread2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ thread2/cmake-build-debug/CMakeFiles/thread2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for thread2.
+# This may be replaced when dependencies are built.
