14cef34300c7d5493289c8580f65761d0d7a54ce
leikang123 <1013626230@qq.com>
2021-11-08 17:23:06 +0800

    demo

---
+++ thread02/cmake-build-debug/CMakeFiles/thread02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread02.dir/main.c.o"
+  "thread02"
+  "thread02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/thread02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ thread02/main.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define NUMBER_OF_THREADS 10
+
+int main(int argc,char argv[]){
+
+ pthread_t threads[NUMBER_OF_THREADS];
+ int status;
+ int i;
+
+    for(i=0;i<NUMBER_OF_THREADS;i++){
+        printf("Main here,Createing thread %d0",i);
+        status = pthread_create(&threads[i],NULL,print_hello_world),(void *)i);
+
+        if(status !=0){
+            printf("Oops.pthread_create returnd error code %d0",status);
+            exit(-1);
+        }
+    }
+    exit(NULL);
+}
+void *print_hello_world(void *tid){
+    printf("Hello world,Greeetings from thread %d0",tid);
+    pthread_exit(NULL);
+}
