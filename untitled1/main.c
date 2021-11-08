110ad54141d9115063651ba10fd50e6681845fc7
leikang123 <1013626230@qq.com>
2021-11-08 17:31:32 +0800

    demo

---
+++ untitled1/main.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void exit(int i);
+
+void wait(void *pVoid);
+
+int main() {
+    pid_t pid;
+    pid = fork();
+    if (pid < 0){
+        fprintf(stderr,"Fork Failed");
+        // 退出-1
+        exit(-1);
+    }else if(pid ==0){
+        execlp("/bin/ls","ls",NULL);
+    }else {
+        // d等待
+        wait(NULL);
+        printf("Clild Complete");
+        // 0退出
+        exit(0);
+    }
+}
+
+void wait(void *pVoid) {
+    printf("等待。。。。");
+
+}
+
+void exit(int i) {
+    printf("退出。。。。");
+}
