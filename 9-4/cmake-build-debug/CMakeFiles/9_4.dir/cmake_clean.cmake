5e6d012276d7f644276526138ebf2e9b5060d6f8
leikang123 <1013626230@qq.com>
2021-12-31 18:14:39 +0800

    update

---
+++ 9-4/cmake-build-debug/CMakeFiles/9_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "9_4"
+  "9_4.pdb"
+  "CMakeFiles/9_4.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9-4/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+# include <string>
+using namespace  std;
+class number{
+    int num;
+public :
+    number( int i){
+    num =i;
+}
+friend int operator ++(number &);
+friend int operator ++(number &,int );
+void print(){
+    cout << "num = " << num << endl;
+}
+};
+int operator ++ (number & a){
+    a.num++;
+    return a.num++;
+}
+int operator ++(number & a ,int ){
+    int i = a.num++;
+    return i;
+}
+int main() {
+    number n(19);
+    int i= ++n;
+    cout << "i=" <<i << endl;
+    n.print();
+    i =n++;
+    cout << "i=" << i << endl;
+    n.print();
+}
