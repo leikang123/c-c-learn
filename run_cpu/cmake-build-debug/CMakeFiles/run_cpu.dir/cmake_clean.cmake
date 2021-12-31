89b9392c005a91ee57af86a7099a64b892f724a6
leikang123 <1013626230@qq.com>
2021-12-31 18:15:48 +0800

    update

---
+++ run_cpu/cmake-build-debug/CMakeFiles/run_cpu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_cpu.dir/main.cpp.o"
+  "run_cpu"
+  "run_cpu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
