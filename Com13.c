2db9d1a258774bf9961b1aa1a64d611133eb1df1
leikang123 <1013626230@qq.com>
2022-01-14 18:46:21 +0800

    demo

---
+++ Com13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define FLAG 1
+int main()
+{
+    char s[80];
+    int i;
+    gets(s);
+
+    #if FLAG
+    for(i=0;s[i]!=0;i++)
+     if(s[i] >'a' && s[i] < 'z')
+     s[i] = s[i]-32;
+     #else
+     for(i=0;s[i]!=0;i++)
+     if(s[i]>'A' && s[i]< 'Z')
+     s[i] = s[i]+32;
+
+     #endif
+     puts(s);
+
+}
