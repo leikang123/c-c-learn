594356c4d626982c6a0c24d62accbbc244f95b4e
leikang123 <1013626230@qq.com>
2022-01-18 18:34:33 +0800

    demo

---
+++ Com21.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int (*p)[4] = a;
+    int i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            printf("%3d",*(*(p+i)+j));
+        }
+        printf("\n");
+    }
+}
