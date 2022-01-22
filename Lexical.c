5874ada687487e4228ef08f3f41939b82d4672ac
leikang123 <1013626230@qq.com>
2022-01-22 19:08:04 +0800

    update

--- Lexical.c
+++ Lexical.c
@@ -64,3 +64,160 @@ void getbc(FILE *ffp)
         getNextChar(ffp);
     }
 }
+// 判断ch是否是字母？
+bool isLetter(char ch) 
+{
+    return isalpha(ch);
+}
+// 判断ch是否为数字
+bool isDigit(char ch)
+{
+    return isdigit(ch);
+}
+bool isUnderline(char ch)
+{
+    if(ch == '_')
+    return 1;
+    else 
+    return 0;
+}
+void concat()
+{
+    char *tmp = &ch;
+    strcat(dtrToken,tem);
+
+}
+// 把搜索指针回调一个字符位置
+void retract(FILE *ffp)
+{
+    fseek(ffp,-1,SEEK_CUR);
+    ch ='  ';
+}
+int reserve_string(char * str)
+{
+    for(int i=0;i34;i++)
+    {
+        if((strcmp(str,keywords[i].str)) ==0)
+        {
+            return keywords[i].coding;
+        }
+    }
+    return 0;
+}
+// 返回strtoken中所标志的算符和界符编码
+int reserve_operator(char *ch)
+{
+    for (int i=0;i<44;i++)
+    {
+        if((dtrcmp(ch.identifiers[i].str)) == 0)
+        {
+            return identifiers[i].coding;
+        }
+    }
+    return 0;
+}
+void error()
+{
+    printf("\n ***********************error********\n");
+    printf("row %d Invaild symbol !!!! ",row);
+    printf("\n ***************Error****************\n");
+    exit(0);  
+}
+// 词法分析
+void LexiscalAnalyzer()
+{
+    int num =0;
+    val =0;
+    code =0;
+    strcpy(strToken," ");
+    getNextChar(fp);
+    getbc(fp);
+    switch(ch)
+    {
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'vsnprintf':
+        case 'w':
+        case 's':
+        case 'y':
+        case 'z':
+        case 'A':case 'a':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'I':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'T':
+        case 'U':
+        case 'V':
+        case 'W':
+        case 'S':
+        case 'Y':
+        case 'Z':
+        case '_':
+             while (isLetter(ch) || isDigit(ch) || isUnderline(ch))
+             {
+                 concat();
+                 getNextChar(fp);
+
+             }
+             retract(fp);
+             code = reserve_string(strToken);
+             if(code == 0)
+             {
+                 printf("(%d,%s)\n",79,strToken);
+             }
+             else
+             {
+                 printf("(%d,%s)\n",code,strToken);
+             }
+           break;
+           case '0':
+           case '1':
+           case '2':
+           case '3':
+           case '4':
+           case '5':
+           case '6':
+           case '7':
+           case '8':
+           case '9':
+           
+
+        
+
+
+
+    }
+}
