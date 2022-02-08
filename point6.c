71d0f8239f4e1f3adaab31b321cfc297f689b46e
leikang123 <1013626230@qq.com>
2022-02-08 20:38:04 +0800

    demo

---
+++ point6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int *p;
+    int **q;
+    p =&a;
+    q=&p;
+    scanf("%d",*q);
+    printf("%d   %d   %d\n",a,*p,**q);
+    char *name [5]={"beijing","wuhan","tianjin","shanghai","nanjing"};
+    char **p3 = name;
+    int i =0;
+    for(i=0;i<5;i++,p3++)
+    {
+        //printf("%s\n",*p3);
+        printf("%s\n",(*p3+i));
+        //p3++;
+    } 
+    
+}
