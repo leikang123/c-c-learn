da021d705ce14fc6ec922dbcbf77455a29976051
leikang123 <1013626230@qq.com>
2022-02-10 17:05:31 +0800

    demo

---
+++ point14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char s[20] ={"leikang"};       // 定义一个字符型数组变量名s
+    puts(s);                       // 用函数输出数组变量名s
+    puts("we ll leikang is ");
+    char s2[] = "zhongguoren";
+    gets(s2);
+    puts(s2);
+
+}
