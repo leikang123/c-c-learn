446230b5e5cfdcbc1e0006c325eb0bc4c35d9773
leikang123 <1013626230@qq.com>
2022-02-11 18:47:20 +0800

    update

---
+++ 9-1.cc/cmake-build-debug/CMakeFiles/9_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "9_1"
+  "9_1.pdb"
+  "CMakeFiles/9_1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9-1.cc/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>             //头文件，表示标准库，输入输出对像
+#include <string>               //头文件，表示字符标准库
+using namespace std;            //使用命名空间
+class str{
+private:
+    char *st;
+public :
+    str( char * s);
+    str( str & s);
+    str& operator = (str& a);
+    str& operator = (char *s);
+    void print(){
+        delete st;
+    }
+};
+str::str(char *s){
+    st = new char[strlen(s) +1];
+    strcpy(st,s);
+
+}
+str :: str(str & a){
+    st = new char[strlen(a.st)+1];
+    strcpy(st,a.st);
+
+}
+str& str ::operator=(str &a) {
+    if (this == & a)
+        return * this;
+    delete st;
+    st = new char[strlen(a.st)+1];
+    strcpy(st,a.st);
+    return * this;
+}
+str& str :: operator=(char  &s) {
+    delete st;
+    st = new char[strlen(s)+1];
+    strcpy(st,s);
+    return * this;
+}
+
+int main() {
+    str s1 ("we"),s2("They"),s3(s1);
+    s1.print();
+    s2.print();
+    s3.print();
+    s2=s1=s3;
+    s3=" go home";
+    s3 = s3;
+    s1.print();
+    s2.print();
+    s3.print();
+}
