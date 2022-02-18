9d10bf5ace079aa4a19a53d042c3b69b075816a1
leikang123 <1013626230@qq.com>
2022-02-18 17:32:50 +0800

    modify file

--- lk65.cpp
+++ lk65.cpp
@@ -1,12 +1,14 @@
-#include <glew.h> 
-#include <glfw3.h>
+#include <GL/glew.h> 
+#include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
-void init(GLFWwindow* window) { }
-void display(GLFWwindow* window, double currentTime) { 
+void init(GLFWwindow* window) { 
+
+}
+    void display(GLFWwindow* window, double currentTime) { 
     glClearColor(1.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-}
+   }
 int main(void) {
 if (!glfwInit()) { 
     exit(EXIT_FAILURE); 
@@ -25,7 +27,7 @@ if (glewInit() != GLEW_OK) {
         display(window, glfwGetTime());
         glfwSwapBuffers(window);
         glfwPollEvents();
-}
+    }
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
